a4def16278efe4c8e0cfd7202dfbfb15be454a60
Juan Saavedra <saavedracastrillojuanariel@gmail.com>
2026-06-17 11:20:33 -0500

    corrected some spaces between lines

--- calorie tracker/calorie_tracker.py
+++ calorie tracker/calorie_tracker.py
@@ -21,6 +21,7 @@ def load_data():
     try:
         with open("users.json", "r") as file:
             entries = json.load(file)
+            
     except FileNotFoundError:
         entries = []
 
